a9179e23207dab0e8924dda7f684ed0d345b6064
Pedro Ventura <pedrojventura@gmail.com>
2017-04-03 18:16:16 +0200

    improve main pizzas list

--- resources/views/pizzas/index.blade.php
+++ resources/views/pizzas/index.blade.php
@@ -1,9 +1,31 @@
-	<ul>	
-	@foreach ($pizzas as $pizza)
-	<li>
-		<a href="/pizzas/{{$pizza->id}}">
-			{{$pizza->name}}
-		</a>
-	</li>
-	@endforeach
-	</ul>
+	@extends('layout')
+
+	@section ('content')
+	
+	<div class="row">
+		<div class="col-12">
+			<h1>Pizzas List</h1>
+		</div>
+	</div>
+	
+	<div class="row">
+		<div class="col-11">
+			<ul class="list-group">
+			@foreach ($pizzas as $pizza)
+				<li class="list-group-item justify-content-between">
+					<a href="/pizzas/{{$pizza->id}}">
+					{{$pizza->name}}
+					</a>
+					<div class="bd-example" data-example-id="">
+						<a role="button" href="/pizzas/edit/{{$pizza->id}}" class="btn btn-primary btn-sm">Edit</a>
+						<a role="button" class="btn btn-danger btn-sm">Delete</a>
+					</div>
+			@endforeach
+			</ul>
+		</div>
+		<div class="col-1">
+			<a class="btn btn-success" href="/pizzas/create" role="button">Create</a>
+		</div>
+	</div>
+	
+	@endsection
