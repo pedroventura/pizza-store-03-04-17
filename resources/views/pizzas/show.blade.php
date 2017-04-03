de8752bb982d010ddcd3ec7692a9c5747a8a058b
Pedro Ventura <pedrojventura@gmail.com>
2017-04-03 18:30:57 +0200

    improve view for pizza details

--- resources/views/pizzas/show.blade.php
+++ resources/views/pizzas/show.blade.php
@@ -1,15 +1,43 @@
-<h1>{{ $pizza->name }}</h1>
-<br />
-Ingredients:
-<br />
-<ul>	
-	@foreach ($ingredients as $ingredient)
-	<li>
-		{{$ingredient}}
-		
-	</li>
-	@endforeach
-</ul>
+@extends('layout')
 
-Price: {{$totalPrice}}
+@section ('content')
+<div class="row">
+	<div class="col-12">
+		<h1>Pizza details & price</h1>
+	</div>
+</div>
 
+
+
+<div class="row">
+	<div class="col-6">
+		<div class="table-responsive">
+			<table class="table table-striped">
+				<thead>
+					<tr>
+					<th>Name</th>
+					<th>Price</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td><p>{{ $pizza->name }}</p></td>
+						<td><p>{{ $totalPrice }}</p></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+	<div class="col-6">
+		<p>Ingredients</p>
+		<ul class="list-group">
+			@foreach ($ingredients as $ingredient)
+				<li class="list-group-item justify-content-between">
+					{{$ingredient}}
+				</li>
+			@endforeach
+		</ul>
+	</div>
+	<p><a class="btn btn-primary" href="/pizzas" role="button"><< Back</a></p>
+</div>
+@endsection
