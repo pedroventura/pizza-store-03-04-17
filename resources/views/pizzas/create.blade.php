98fa6aec9444a78cd99cd5f8d0410602be232370
Pedro Ventura <pedrojventura@gmail.com>
2017-04-03 18:36:37 +0200

    improve create pizza view

--- resources/views/pizzas/create.blade.php
+++ resources/views/pizzas/create.blade.php
@@ -1,32 +1,38 @@
+@extends('layout')
+
+@section ('content')
+
 <form method="POST" action="/pizzas">
 
 	{{ csrf_field() }}
-  <div class="form-group">
-    <label for="name">Name</label>
-    <input type="text" class="form-control" name="name" id="name" placeholder="Pizza Name">
-  </div>
-  
-  <div class="form-group">
-    <label for="price">Pricel</label>
-    <input type="file" class="form-control" id="image" name="image" placeholder="Ingredient image">
-  </div>
-  <div class="form-group">
-    <label for="price">Ingredients</label>
-    <ul>	
-	@foreach ($ingredients as $ingredient)
-	<li>
-		<?php
-		/**
-		* @todo use a Form provider
-		*/
-		?>
-		<label>{{$ingredient->name}}</label>
-		<input name="{{$ingredient->name}}" type="checkbox" value="">	
-	</li>
-	@endforeach
-	</ul>
-  </div>
+	<div class="form-group">
+		<label for="name">Name</label>
+		<input type="text" class="form-control" name="name" id="name" placeholder="Pizza Name">
+	</div>
 
-  <button type="submit" class="btn btn-primary">Submit</button>
+	<div class="form-group">
+		<label for="Image">Image</label>
+		<input type="file" class="form-control" id="image" name="image" placeholder="Ingredient Image">
+	</div>
 
+	<div class="form-group">
+		<label for="price">Ingredients</label>
+		<ul class="list-group">
+			@foreach ($ingredients as $ingredient)
+				<li class="list-group-item justify-content-between">
+				<?php
+				/**
+				* @todo use a Form provider
+				*/
+				?>
+				<label>{{$ingredient->name}}</label>
+				<input name="{{$ingredient->name}}" type="checkbox" value="">	
+				</li>
+			@endforeach
+		</ul>
+	</div>
+
+	<button type="submit" class="btn btn-primary">Submit</button>
 </form>
+
+@endsection
